e143b4bf01a0700f23ca088073be63869b747766
Tarun yadav <tarunyadav9761@gmail.com>
2023-09-09 10:24:26 +0530

    Refactor code and improve formatting

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-const Mustache = require("mustache");
-const fs = require("fs");
-const fetch = require('node-fetch');
-require('dotenv').config();
-const MUSTACHE_MAIN_DIR = "./main.mustache";
+const Mustache = require('mustache')
+const fs = require('fs')
+const fetch = require('node-fetch')
+require('dotenv').config()
+const MUSTACHE_MAIN_DIR = './main.mustache'
 
 const DATA = {
-    name: "Tarun Yadav",
-    date: new Date().toLocaleDateString("en-GB", {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        timeZoneName: "short",
-        timeZone: "Europe/Stockholm",
-    }),
-    surename: "Yadav ha",
-    location: 'Delhi, India',
-    temperature: '',
-};
-
+	name: 'Tarun Yadav',
+	date: new Date().toLocaleDateString('en-GB', {
+		weekday: 'long',
+		month: 'long',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		timeZoneName: 'short',
+		timeZone: 'Europe/Stockholm',
+	}),
+	surename: 'Yadav',
+	location: 'Delhi, India',
+	temperature: '',
+}
 
 const weather = async () => {
-    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=${process.env.API}&units=metric`)
+	return await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=${process.env.API}&units=metric`
+	)
 }
 
-weather().then(res => res.json())
-    .then(weatherData => DATA.temperature = weatherData.main.temp)
-    .then(() => generateReadMe())
-
+weather()
+	.then((res) => res.json())
+	.then((weatherData) => (DATA.temperature = weatherData.main.temp))
+	.then(() => generateReadMe())
 
 function generateReadMe() {
-    fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
-        if (err) throw err;
-        const output = Mustache.render(data.toString(), DATA);
-        fs.writeFileSync("README.md", output);
-    });
+	fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+		if (err) throw err
+		const output = Mustache.render(data.toString(), DATA)
+		fs.writeFileSync('README.md', output)
+	})
 }
-
-
